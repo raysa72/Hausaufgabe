a11404b7c78983e353a8aa1e8d399f114221a234
raysa72 <142941252+raysa72@users.noreply.github.com>
2023-09-10 20:53:19 +0200

    Initial commit

---
+++ src/main/java/de/telran/Main.java
@@ -0,0 +1,10 @@
+package de.telran;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello_java");
+
+        
+
+    }
+}
---
+++ src/main/java/de/telran/lesson2/Main2.java
@@ -0,0 +1,4 @@
+package de.telran.lesson2;
+
+public class Main2 {
+}
