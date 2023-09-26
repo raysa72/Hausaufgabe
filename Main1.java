e3d43c1c96c231d1d3f8d07f38939b4509fe2d41
raysa72 <142941252+raysa72@users.noreply.github.com>
2023-09-26 11:39:07 +0200

    Add files via upload

---
+++ Main1.java
@@ -0,0 +1,22 @@
+package Package5;
+
+import java.util.Scanner;
+
+public class Main1 {
+    public static void main(String[] args) {
+
+        String str1 = "42";
+        String str2 = "49";
+
+         int num1 = Integer.parseInt(str1);
+         int num2 = Integer.parseInt(str2);
+
+         String result = " Дорогая Раиса , в свои " + str1 + " лет Вы для нас дороги , как " + str2 + " килограммов Золота !";
+         System.out.println(result);
+
+
+
+        }
+
+    }
+
